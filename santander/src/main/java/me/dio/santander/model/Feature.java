c62a45acc3757f6e4f1fd5e473b69650798d4c65
Gabriel Silva88 <gabrielmg.silva@outlook.com>
2025-03-17 18:51:40 -0300

    Alteração

---
+++ santander/src/main/java/me/dio/santander/model/Feature.java
@@ -0,0 +1,15 @@
+package me.dio.santander.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem{
+
+	
+	
+}
